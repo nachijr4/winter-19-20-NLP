6e1bb80fb3882af10e5fab4a32c6e573a4fbb103
Nachimuthu <nachi2muthu13@gmail.com>
2019-12-06 23:20:16 +0530

    Excercise 1

---
+++ 6-12-19/Excercise1.py
@@ -0,0 +1,21 @@
+text = "Shakespeare gave the best theatrics in english"
+print(text)
+
+text = '''William Shakespeare (bapt. 26 April 1564 – 23 April 1616)
+ was an English poet, playwright, and actor, 
+ widely regarded as the greatest writer in the English 
+ language and the world's greatest dramatist.
+  He is often called England's national poet and the "Bard of Avon" (or simply "the Bard"). 
+  His extant works, including collaborations, consist of some 39 plays,[c] 154 sonnets, 
+  two long narrative poems, and a few other verses, some of uncertain authorship. 
+  His plays have been translated into every major living language and are performed 
+  more often than those of any other playwright'''
+
+print(text)
+
+text = '''भारत के पहले प्रधानमंत्री रहे पंडित जवाहरलाल नेहरू का जन्म 14 
+  नवंबर 1889 इलाहाबाद में हुआ था। उनका जन्मदिन 
+  बाल दिवस के रूप में मनाया जाता है। उनके पिता का नाम मोतीलाल नेहरू था, 
+  जो एक धनाढ्य परिवार के थे और माता का नाम स्वरूपरानी'''
+
+print(text)
